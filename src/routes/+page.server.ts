e0d0a1fb7125c0fcd3fc95aa650cfca4abd0ec0d
Maximilian Kürschner <dev@maximilian-kuerschner.de>
2023-05-06 09:57:57 +0200

    feat: simple update donut form action

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,24 @@
+import type { Actions, PageServerLoad } from './$types';
+
+export const actions: Actions = {
+    update: async ({request}) => {
+        const data = await request.formData();
+        console.log(...data);
+        
+        
+        // save the data to the database e.g. with pocketbase
+        // const record = await pb.collection('donuts').update(data.get('id'), data);
+    }
+};
+
+export const load: PageServerLoad = async ({ params }) => {
+    // load the data from the database e.g. with pocketbase
+    // const record = await pb.collection('donuts').get(params.id);
+    // return { donut: record };
+    return { donut: {
+        id: "vanille-donut",
+        name: "Vanille Donut",
+        description: "A donut with vanilla flavor",
+        price: 1,
+    }};
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    export let data;
+</script>
+
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@1/css/pico.min.css">
+</svelte:head>
+
+<section class="container">
+    <h1>Edit {data.donut.name}</h1>
+
+    <form method="POST" action="?/update">
+        <label for="name">Name</label>
+        <input type="text" name="name" id="name" value="{data.donut.name}">
+
+        <label for="price">Price</label>
+        <input type="number" name="price" id="price" value="{data.donut.price}">
+
+        <label for="description">Description</label>
+        <textarea name="description" id="description">{data.donut.description}</textarea>
+
+        <button type="submit">Save</button>
+    </form>
+</section>
